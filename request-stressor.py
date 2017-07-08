891856d2588d74ff1cfeffaec12428003c75490d
Richard Huynh <huynhtastic@gmail.com>
2017-07-08 18:48:19 -0500

    more verbose output; upped clients to 1000 and each only makes 1 request

--- request-stressor.py
+++ request-stressor.py
@@ -1,16 +1,27 @@
+"""
+add async requesting
+refactor to show time to request
+
+"""
 import requests
 import threading
+import timeit
 
 def make_requests(i):
-    while True:
-        print(i, requests.get('http://www.projectonyx.io'))
+#    while True:
+    start_time = timeit.default_timer()
+    requests.get('http://www.projectonyx.io')
+    exec_time = timeit.default_timer() - start_time
+    print('Client {0:2} Response time: {1}'.format(i, exec_time))
 
 def main():
-    for i in range(40):
+    num_clients = 1000
+    start_test = timeit.default_timer()
+    for i in range(num_clients):
         t = threading.Thread(target=make_requests, args=(i,))
         t.start()
+    exec_test = timeit.default_timer() - start_test
+    print('{0} clients in {1} seconds'.format(num_clients, exec_test))
 
-print(__name__)        
-        
 if __name__ == '__main__':
     main()
